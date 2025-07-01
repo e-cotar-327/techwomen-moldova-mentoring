f36d2e8224989f3ea243ba728c16e010b35f0db8
Alexandra123713 <alexandracerevatii@gmail.com>
2025-07-01 18:19:11 +0300

    Navbar: modified styles

--- public/reusable/reusableComponents.js
+++ public/reusable/reusableComponents.js
@@ -1,4 +1,16 @@
-const loadComponent = (selector, file) => {
+function setActiveNavLink() {
+    const currentPage = location.pathname.split('/').pop() || 'index.html';
+    const links = document.querySelectorAll('.nav-link');
+
+    links.forEach((link) => {
+        const href = link.getAttribute('href');
+        if (href === currentPage) {
+            link.classList.add('active');
+        }
+    });
+}
+// Show reusable components
+const loadComponent = (selector, file, callback) => {
     fetch(file)
         .then((response) => {
             if (!response.ok) {
@@ -8,11 +20,12 @@ const loadComponent = (selector, file) => {
         })
         .then((data) => {
             document.querySelector(selector).innerHTML = data;
+            if (callback) callback();
         })
         .catch((error) => console.error(error));
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-    loadComponent('#navbar', 'reusable/navbar.html');
+    loadComponent('#navbar', 'reusable/navbar.html', setActiveNavLink);
     loadComponent('#footer', 'reusable/footer.html');
 });
